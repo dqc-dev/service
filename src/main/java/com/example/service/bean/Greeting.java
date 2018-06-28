1e87418ad2bcf53126254ee5b3b592e91a495736
soleray <pein_zero@126.com>
2018-06-28 09:26:07 +0800

    ws init

---
+++ src/main/java/com/example/service/bean/Greeting.java
@@ -0,0 +1,13 @@
+package com.example.service.bean;
+
+public class Greeting {
+    private String content;
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
