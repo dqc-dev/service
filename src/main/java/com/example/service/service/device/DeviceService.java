fba95b190a25293b1f6d0c2f53a7a9cf45e9cb39
soleray <pein_zero@126.com>
2018-07-02 16:26:22 +0800

    ws init

--- src/main/java/com/example/service/service/device/DeviceService.java
+++ src/main/java/com/example/service/service/device/DeviceService.java
@@ -1,40 +1,12 @@
 package com.example.service.service.device;
 
-import com.globalvillage.bg.common.bean.page.PageList;
-import com.globalvillage.bg.common.dto.device.DeviceListDTO;
-import com.globalvillage.bg.common.dto.device.WebActivedDevListDTO;
-import com.globalvillage.bg.common.mybatis.entity.device.Device;
-import com.globalvillage.bg.common.service.base.IService;
 
-import java.util.List;
+import com.example.service.entity.device.Device;
+import com.example.service.service.base.IService;
 
 /**
  * Created by Arthur on 2017/3/14 0014.
  */
 public interface DeviceService extends IService<Device> {
 
-    PageList<DeviceListDTO> listUserDevices(Long id);
-
-    void changeStrainer(String userToken, String deviceSn, String newStrainerSn, String strainerModelCode) throws Exception;
-
-    PageList<WebActivedDevListDTO> listActivedDevice(long categoryId, Long companyId, String deviceSn, long start, int limit);
-
-    void scanPage(Long appUserId, String deviceSn, String pageId) throws Exception;
-
-    DeviceListDTO getDeviceDetail(long deviceId, Long appUserId);
-
-    Device selectBySn(String devSn);
-
-    void clearAddress(long deviceId);
-
-    void activeDevice(Long deviceId, Long deviceBatchId);
-
-    List<Device> checkActive(List<Device> devList);
-
-
-    List<Device> selectActivedDevice();
-
-    List<Device> selectActivedAndOnlineDevice();
-
-    String getDeviceCode(String sn) throws Exception;
 }
